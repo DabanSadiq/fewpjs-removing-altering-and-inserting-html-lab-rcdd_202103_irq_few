d0d111a6c4db49671c84b896d463f98ab64b51d4
Daban Sadiq <dabansadiq@icloud.com>
2021-04-06 18:38:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let element = createElement('p')
-let body = getElementsByTag('body')[0];
-let main = getElementByID('main');
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Daban \'is the champion\' ";
+let body = document.getElementsByTagName('body')[0];
+let main = document.getElementById('main');
 main.remove();
-element.innerHTML = 'hello';
+
 document.body.appendChild(element);
 element.style.color = 'red';
