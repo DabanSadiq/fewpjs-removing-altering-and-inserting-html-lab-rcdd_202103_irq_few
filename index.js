1846d1cceb23929a738c787bac89b59b0a5e8f07
Daban Sadiq <dabansadiq@icloud.com>
2021-04-06 18:33:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let element = createElement('p')
+let body = getElementsByTag('body')[0];
+let main = getElementByID('main');
+main.remove();
+element.innerHTML = 'hello';
+document.body.appendChild(element);
+element.style.color = 'red';
